57182ce5557fcb94b80e9ab64888e2b611f725ad
Hasina Rina <hasinarina92@gmail.com>
2025-12-16 09:01:16 +0300

    16/12/25 Mise a jour 1

--- ans-design-backoffice/config/db.php
+++ ans-design-backoffice/config/db.php
@@ -1,8 +1,8 @@
 <?php
-$host = 'localhost';  
-$dbname = 'nemo5160_ans_design';
-$user = 'nemo5160_ans_design-admin';
-$pass = '.;k)lp+K5+sL'; 
+$host = '127.0.0.1';
+$dbname = 'ans_design';
+$user = 'root';
+$pass = ''; 
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $pass);
